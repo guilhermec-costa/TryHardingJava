2897a63f386e6da0867d5ee13a67dc52c7c94eff
Guilherme China <echina725@gmail.com>
2024-04-08 21:03:02 -0300

    initial commit

---
+++ javaIntroduction/java-intro/src/main/java/com/javaintro/javaintro/JavaIntroApplication.java
@@ -0,0 +1,9 @@
+package com.javaintro.javaintro;
+
+public class JavaIntroApplication {
+
+	public static void main(String[] args) {
+        System.out.println("Hello world");
+	}
+
+}
---
+++ maven-test/src/main/java/br/com/tests/app/App.java
@@ -0,0 +1,13 @@
+package br.com.tests.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
