fa9f05a02af392de5cdb9e319a324532e6fc7d26
Guilherme China <echina725@gmail.com>
2024-04-10 21:37:38 -0300

    json parser refactoring

--- jacksonIntro/src/main/java/com/javaIntro/jacksonIntro/JsonParser.java
+++ jacksonIntro/src/main/java/com/javaIntro/jacksonIntro/JsonParser.java
@@ -3,7 +3,6 @@ package com.javaIntro.jacksonIntro;
 import java.io.IOException;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.PrettyPrinter;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -11,7 +10,7 @@ import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
 public class JsonParser {
-    private static ObjectMapper defaultObjectMapper = generateConfiguredObjectMapper();
+    private static ObjectMapper objectMapper = generateConfiguredObjectMapper();
 
     private static ObjectMapper generateConfiguredObjectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
@@ -20,25 +19,29 @@ public class JsonParser {
     }
 
     public static JsonNode fromJsonStringToJsonNode(String json) throws IOException {
-        return defaultObjectMapper.readTree(json);
+        return objectMapper.readTree(json);
     }
 
     public static <T> T fromJsonNodeToObject(JsonNode jsonNode, Class<T> _class) throws JsonProcessingException {
-        return defaultObjectMapper.treeToValue(jsonNode, _class);
+        return objectMapper.treeToValue(jsonNode, _class);
     }
 
     public static JsonNode fromObjectToJsonNode(Object object) {
-        return defaultObjectMapper.valueToTree(object);
+        return objectMapper.valueToTree(object);
     }
 
     public static String fromJsonNodeToJsonString(JsonNode jsonNode) throws JsonProcessingException  {
-        ObjectWriter objectWriter = defaultObjectMapper.writer();
-        return objectWriter.writeValueAsString(jsonNode);
+        return generateString(jsonNode, false);
     }
 
     public static String prettifyJsonNode(JsonNode jsonNode) throws JsonProcessingException {
-        ObjectWriter objectWriter = defaultObjectMapper.writer();
-        objectWriter = objectWriter.with(SerializationFeature.INDENT_OUTPUT);
+        return generateString(jsonNode, true);
+    }
+
+    private static String generateString(JsonNode jsonNode, Boolean shouldPrettify) throws JsonProcessingException {
+        ObjectWriter objectWriter = objectMapper.writer();
+        if(shouldPrettify) objectWriter = objectWriter.with(SerializationFeature.INDENT_OUTPUT);
         return objectWriter.writeValueAsString(jsonNode);
     }
+
 }
