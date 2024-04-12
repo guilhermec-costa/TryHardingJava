9c644f2575210b107ac9ecbe7d7d6de381ea0409
Guilherme China <echina725@gmail.com>
2024-04-11 21:07:47 -0300

    adds JDBC project

---
+++ JDBCIntro/src/main/java/com/javaIntro/JDBCIntro/App.java
@@ -0,0 +1,13 @@
+package com.javaIntro.JDBCIntro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
