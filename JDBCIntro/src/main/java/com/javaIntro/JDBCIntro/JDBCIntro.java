de64033a10600866945f88efe2c5393038f25b7e
Guilherme China <echina725@gmail.com>
2024-04-13 09:39:46 -0300

    it is finally working. I learn a bunch of things

--- JDBCIntro/src/main/java/com/javaIntro/JDBCIntro/JDBCIntro.java
+++ JDBCIntro/src/main/java/com/javaIntro/JDBCIntro/JDBCIntro.java
@@ -12,14 +12,13 @@ public class JDBCIntro
 {
     public static void main( String[] args ) throws Exception
     {   
-        // Class.forName("org.h2.Driver"); 
-        String classpath = System.getProperty("java.class.path");
-        System.out.println("Classpath: " + classpath);
-        // try ( Connection connection = DriverManager.getConnection("jdbc:h2:mem:")) {
-        //     System.out.println(connection.isValid(0));
-        // } catch(SQLException error) {
-        //
-        //     System.out.println(error.getMessage());
-        // }
+        Class.forName("org.h2.Driver");
+        try ( Connection connection = DriverManager.getConnection("jdbc:h2:mem:")) {
+            System.out.println(connection.isValid(0));
+            System.out.println("Hello world");
+        } catch(SQLException error) {
+
+            System.out.println(error.getMessage());
+        }
     }
 }
