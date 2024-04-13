5b0a1cbd17d96ce86c1a16a002ee548644cb1e46
Guilherme China <echina725@gmail.com>
2024-04-13 20:17:25 -0300

    adds test for database connection

--- JDBCIntro/src/test/java/com/javaIntro/JDBCIntro/AppTest.java
+++ JDBCIntro/src/test/java/com/javaIntro/JDBCIntro/AppTest.java
@@ -1,20 +1,37 @@
 package com.javaIntro.JDBCIntro;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Logger;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+    private String connectionString = "jdbc:h2:mem:";
+    Logger logger = Logger.getLogger("JDBCIntro");
+    
     @Test
-    public void shouldAnswerWithTrue()
+    public void establishH2DatabaseConnection()
     {
-        assertTrue( true );
+        try ( Connection databaseConnection = DriverManager.getConnection(connectionString)) {
+            Boolean isConnectionValid = databaseConnection.isValid(0);
+            assertEquals(true, isConnectionValid);
+        } catch(SQLException error) {
+            logger.info(error.getMessage());
+        }
+    }
+
+    @Test
+    public void insertRow() {
+        try ( Connection databaseConnection = DriverManager.getConnection(connectionString)) {
+            PreparedStatement preparedStatement = databaseConnection.prepareStatement("select * from users");
+        } catch(SQLException error) {
+        }
     }
 }
