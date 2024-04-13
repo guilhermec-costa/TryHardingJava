84ba13f7c435eb74b04cc7cdeaeff900ea557a0a
Guilherme China <echina725@gmail.com>
2024-04-13 10:39:37 -0300

    add script to add classpath automatically

--- JDBCIntro/scripts/run.sh
+++ JDBCIntro/scripts/run.sh
@@ -1,1 +1,23 @@
-java -cp target/classes:lib/h2-2.2.224.jar com.javaIntro.JDBCIntro.JDBCIntro
+#!/sbin/env bash
+
+dependencies=(
+    "target/classes"
+    "lib/h2-2.2.224.jar"
+)
+
+main_class=com.javaIntro.JDBCIntro.JDBCIntro
+dependencies_string=""
+dependencies_length=${#dependencies[@]}
+
+for ((i=0; i<=$dependencies_length; i++)); do
+    if [[ $i -eq 0 ]]; then
+        dependencies_string+="${dependencies[$i]}"
+        continue
+    fi
+
+    dependencies_string+=":${dependencies[$i]}"
+done
+#
+echo $dependencies_string
+java -cp $dependencies_string $main_class
+
