fda8cd6a662794f227d12c9927b48c51af591ca1
Guilherme China <echina725@gmail.com>
2024-04-13 12:16:30 -0300

    add resources directory

--- JDBCIntro/src/main/java/com/javaIntro/JDBCIntro/JDBCIntro.java
+++ JDBCIntro/src/main/java/com/javaIntro/JDBCIntro/JDBCIntro.java
@@ -3,7 +3,6 @@ package com.javaIntro.JDBCIntro;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Driver;
 /**
  * Hello world!
  *
@@ -14,6 +13,7 @@ public class JDBCIntro
     {   
         try ( Connection connection = DriverManager.getConnection("jdbc:h2:mem:")) {
             System.out.println(connection.isValid(0));
+            System.out.println("Hello world");
         } catch(SQLException error) {
 
             System.out.println(error.getMessage());
