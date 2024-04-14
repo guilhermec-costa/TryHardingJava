ed4960b7c0c4fcaa7828e83341c4a4f7493fd038
Guilherme China <echina725@gmail.com>
2024-04-14 13:03:03 -0300

    add swing project

---
+++ Swing/Main.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame();
+        jFrame.setTitle("My frame");
+
+        jFrame.setSize(420, 420);
+        jFrame.setVisible(true);
+    }
+}
---
+++ Swing/WindowInitializer.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+/**
+ * WindowInitializer
+ */
+public class WindowInitializer implements Initializer {
+    @Override
+    public void init() {
+
+    }
+}
