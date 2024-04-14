2e6f1936c00db64b75c691c744d2ce19e3ea96d3
Guilherme China <echina725@gmail.com>
2024-04-14 14:16:04 -0300

    refactor project structure

--- Swing/src/Main.java
+++ Swing/src/Main.java
@@ -1,9 +1,13 @@
 import javax.swing.JFrame;
+import java.awt.Color;
 
 public class Main {
     public static void main(String[] args) {
         JFrame window = WindowInitializer.init("Swing Introduction");
         window.setSize(500, 500);
         window.setVisible(true);
+        window.getContentPane().setBackground(Color.CYAN);
+
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
