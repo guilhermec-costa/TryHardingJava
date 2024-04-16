648228bdc0fb9580382d0b9d2edc3c1a8fb5fe37
Guilherme China <echina725@gmail.com>
2024-04-16 19:18:24 -0300

    add jackson dependency

--- lombokIntro/src/main/java/com/javaIntro/lombokIntro/App.java
+++ lombokIntro/src/main/java/com/javaIntro/lombokIntro/App.java
@@ -1,13 +1,9 @@
 package com.javaIntro.lombokIntro;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    {   
+        System.out.println("Hello world");
     }
 }
