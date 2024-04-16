c4a1cc1336d8a9a7d03d62a8a6e376baf7773d0c
Guilherme China <echina725@gmail.com>
2024-04-16 20:46:24 -0300

    suffering again

---
+++ lombokIntro/src/main/java/com/javaIntro/lombokIntro/App.java
@@ -0,0 +1,11 @@
+package com.javaIntro.lombokIntro;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ObjectMapper objectMapper = new ObjectMapper();
+    }
+}
