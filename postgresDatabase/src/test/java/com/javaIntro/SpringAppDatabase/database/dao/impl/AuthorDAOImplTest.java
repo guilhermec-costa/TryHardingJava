54d85c31c7237b16c47726f13b8182860c27ff3b
Guilherme China <echina725@gmail.com>
2024-04-20 12:41:31 -0300

    refactor dao impl tests

---
+++ postgresDatabase/src/test/java/com/javaIntro/SpringAppDatabase/database/dao/impl/AuthorDAOImplTest.java
@@ -0,0 +1,52 @@
+package com.javaIntro.SpringAppDatabase.database.dao.impl;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import static org.mockito.Mockito.verify;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+
+import com.javaIntro.SpringAppDatabase.database.DAOs.Impl.AuthorDAOImpl;
+import com.javaIntro.SpringAppDatabase.database.domain.Author;
+
+/**
+ * AuthorDAOImplTest
+ */
+@ExtendWith(MockitoExtension.class)
+public class AuthorDAOImplTest {
+    
+    @Mock
+    private JdbcTemplate jdbcTemplate;
+    
+    @InjectMocks
+    private AuthorDAOImpl authorDAOImpl;    
+
+    @Test
+    public void testThatCreateAuthorGeneratesCorrectSQL() {
+        Author author = new Author("Churros", 8);
+        authorDAOImpl.create(author);
+        verify(jdbcTemplate).
+            update(
+                eq("insert into authors (name, age) values (?, ?);"),
+                eq("Churros"),
+                eq(8)
+            );
+    }
+
+    @Test
+    public void testThatFindOneAuthorGenetaresCorrectSQL() {
+        authorDAOImpl.findOne(1L);
+        verify(jdbcTemplate).query(
+            "select * from authors where id = ?"
+            any(RowMapper.class),
+            eq(1L)
+        );
+    }
+
+}
